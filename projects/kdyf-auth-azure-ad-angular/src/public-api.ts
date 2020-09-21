5646967b89b95cbe9e2493d19bdd53f2ac7caaca
Ricardo Pari <info@kreadyf.com>
2020-09-21 16:57:31 -0400

    new version

--- projects/kdyf-auth-azure-ad-angular/src/public-api.ts
+++ projects/kdyf-auth-azure-ad-angular/src/public-api.ts
@@ -2,5 +2,13 @@
  * Public API Surface of kdyf-auth-azure-ad-angular
  */
 
+// NGRX
 export * from './lib/auth.module';
 export * from './lib/auth.reducer';
+export * from './lib/auth.actions';
+// MODELS
+export * from './lib/models/auth.models';
+export * from './lib/models/auth-config.model';
+export * from './lib/models/auth-grant-type.enum';
+
+
